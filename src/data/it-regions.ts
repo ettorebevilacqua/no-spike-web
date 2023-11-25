b432915a0a5d5e0515f7ad66ac20a44312cce189
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-11-25 14:48:29 +0100

    data cities

---
+++ src/data/it-regions.ts
@@ -0,0 +1,24 @@
+export const regionsName = {
+    Piemonte: 1,
+    ValleAosta: 2,
+    Lombardia: 3,
+    Trentino: 4,
+    Veneto: 5,
+    Friuli: 6,
+    Liguria: 7,
+    Emilia: 8,
+    Toscana: 9,
+    Umbria: 10,
+    Marche: 11,
+    Lazio: 12,
+    Abruzzo: 13,
+    Molise: 14,
+    Campania: 15,
+    Puglia: 16,
+    Basilicata: 17,
+    Calabria: 18,
+    Sicilia: 19,
+    Sardegna: 20,
+}
+
+export type RegionsName = typeof regionsName;
