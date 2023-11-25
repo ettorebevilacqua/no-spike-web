6dec156819d37a3a082e6d3e2535d8346d836bcd
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-11-25 14:47:13 +0100

    add prj config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,46 @@
-module.exports = {
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:react-hooks/recommended",
-        "prettier"
+/** @type {import("eslint").Linter.Config} */
+const config = {
+    extends: [
+      "turbo",
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended-type-checked",
+      "plugin:@typescript-eslint/stylistic-type-checked",
+      "prettier",
     ],
-    "parser": "@typescript-eslint/parser",
-    "plugins": ["@typescript-eslint"],
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+      es2022: true,
+      node: true,
     },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    }
-}
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: true,
+    },
+    plugins: ["@typescript-eslint", "import"],
+    rules: {
+      "turbo/no-undeclared-env-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports", fixStyle: "separate-type-imports" },
+      ],
+      "@typescript-eslint/no-misused-promises": [
+        2,
+        { checksVoidReturn: { attributes: false } },
+      ],
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+    },
+    ignorePatterns: [
+      "**/.eslintrc.cjs",
+      "**/*.config.js",
+      "**/*.config.cjs",
+      ".next",
+      "dist",
+      "pnpm-lock.yaml",
+    ],
+    reportUnusedDisableDirectives: true,
+  };
+
+  module.exports = config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import svgr from 'vite-plugin-svgr'
+import reactCssModule from "vite-plugin-react-css-modules";
+const generateScopedName = "[name]__[local]___[hash:base64:5]";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(), svgr(), 
+        reactCssModule({
+            generateScopedName,
+            filetypes: {
+              ".less": {
+                syntax: "postcss-less",
+              },
+            },
+          }),
+          reactRefresh()
+    ],
+    css: {
+        modules: {
+          generateScopedName,
+        },
+      },
     define: {
         'process.env': process.env,
     },
